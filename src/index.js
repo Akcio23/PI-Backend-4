4f92ad5760dd0c7eab34bea399be1333001c466d
akcio23 <dev.akcio@gmail.com>
2025-03-17 20:22:30 -0300

    feat: constructor database conection

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
 import express from "express"
 import routerLogin from "./routes/routerLogin.js";
+import cors from "cors"
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+
+
+dotenv.config({ path: '../.env' });
+const dbUrl = process.env.DATABASE_URL;
+console.log(dbUrl)
+
+mongoose.connect(dbUrl)
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Failed to connect to MongoDB', err));
 
 export const app = express()
 
+app.use(cors())
 app.use(express.json());
 app.use(routerLogin)
 
 const PORT = 3000; 
 
-
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log(`Run: ${PORT}`);
 });
 
 export default app 
+
+
